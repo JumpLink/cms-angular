b15c6965383b219a3cea6984583310d79b05ca26
JumpLink <pascal@jumplink.eu>
2015-07-30 14:18:00 +0200

    outsourced from bootstrap theme

---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+angular.module('jumplink.cms.config', [
+    'sails.io',
+  ])
+
+  .service('ConfigService', function ($sailsSocket, $filter, $log) {
+
+
+    var resolve = function(page) {
+      return $sailsSocket.get('/config/find').then (function (data) {
+        return data.data;
+      }, function error (resp){
+        $log.error("Error on resolve "+page, resp);
+      });
+    };
+
+    return {
+      resolve: resolve
+    };
+  });
+
+;
