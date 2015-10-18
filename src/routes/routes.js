2eca62491046284446a5f505759d54862839b0fb
JumpLink <pascal@jumplink.eu>
2015-10-18 16:03:41 +0200

    new jlRoutesProvider

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,200 +1,275 @@
 angular.module('jumplink.cms.routes', [
-    'sails.io',
-    'jumplink.cms.sails',
-    'mgcrea.ngStrap',
-    'jumplink.cms.sortable',
-    'ngFocus',
-    'jumplink.cms.utilities'
-  ])
+  'jumplink.cms.sails',
+  'jumplink.cms.sortable',
+  'jumplink.cms.utilities',
+  'ui.router',                 // AngularUI Router: https://github.com/angular-ui/ui-router
+])
+.provider('jlRoutes', function jlRoutesProvider($stateProvider, $urlRouterProvider, $locationProvider) {
 
-  .filter('showOnlyMainRoutes', function() {
-    // In the return function, we must pass in a single parameter which will be the data we will work on.
-    // We have the ability to support multiple other parameters that can be passed into the filter optionally
-    return function(routes, active) {
-      if(active !== true) {
-        return routes;
-      }
-      var result = [];
-      for (var i = 0; i < routes.length; i++) {
-        if(routes[i].main === true) {
-          result.push(routes[i]);
-        }
-      }
-      return result;
-    };
-  })
-  .service('RoutesService', function ($rootScope, JLSailsService, $log, SortableService) {
-    var create = function(data) {
-      if(!data || !data.match) {
-        data.match = "";
-      }
-      if(!data || !data.title) {
-        data.title = "";
-      }
-      if(!data.main) {
-        data.main = false;
-      }
-      if(!data || !data.state) {
-        data.state = {};
-      }
-      if(!data || !data.url) {
-        data.url = "";
-      }
-      if(!data || !data.alternativeUrls) {
-        data.alternativeUrls = [];
-      }
-      if(!data.state.name) {
-        data.state.name = "";
-      }
-      if(!data.state.customstate) {
-        data.state.customstate = false;
-      }
-      if(!data.state.url) {
-        data.state.url = "";
-      }
-      if(!data.state.views) {
-        data.state.views = "";
-      }
-      if(!data.state.resolve) {
-        data.state.resolve = "";
-      }
-      if(!data || !data.fallback) {
-        data.fallback = {};
-      }
-      if(!data.fallback.url) {
-        data.fallback.url = "";
-      }
-      return data;
-    };
+  this.html5Mode = $locationProvider.html5Mode;
 
-    var append = function(routes, data, callback) {
-      data = create(data);
-      $log.debug("[RoutesService] data", data);
-      SortableService.append(routes, data, callback);
-    };
+  this.otherwise = $urlRouterProvider.otherwise;
 
-    var swap = function(routes, index_1, index_2, callback) {
-      return SortableService.swap(contents, index_1, index_2, callback);
-    };
+  this.state = $stateProvider.state;
 
-    var moveForward = function(index, routes, callback) {
-      return SortableService.moveForward(index, routes, callback);
-    };
-
-    var moveBackward = function(index, routes, callback) {
-      return SortableService.moveBackward(index, routes, callback);
-    };
-
-    var removeFromClient = function (routes, index, route, callback) {
-      if(angular.isFunction(callback)) {
-        return SortableService.remove(routes, index, route, callback);
-      }
-      return SortableService.remove(routes, index, route);
-    };
-
-    var remove = function(routes, index, route, callback) {
-      if((angular.isUndefined(route) || route === null) && angular.isDefined(index)) {
-        route = routes[index];
-      }
-      if(angular.isUndefined(index) || index === null) {
-        index = routes.indexOf(route);
-      }
-      routes = removeFromClient(routes, index, route);
-      // if route has an id it is saved on database, if not, not
-      if(route.id) {
-        $log.debug("[RoutesService.remove] remove from server, too", route);
-        var options = {
-          method: 'delete',
-          resultIsArray: false
-        };
-        return JLSailsService.resolve('/routes/destroy/'+route.id, {id:route.id}, options, callback);
-      }
-    };
-
-    var find = function(query, callback) {
-      // $log.debug("[RoutesService.find]");
-      var options = {
-        method: 'post',
-        resultIsArray: true
-      };
-      return JLSailsService.resolve('/routes/find', query, options, callback);
-    };
-
-    var findOne = function(query, callback) {
-      // $log.debug("[RoutesService.find]");
-      var options = {
-        method: 'post',
-        resultIsArray: false
-      };
-      return JLSailsService.resolve('/routes/findOne', query, options, callback);
-    };
-
-
+  this.setRoutes = function(routes, routeOptions) {
     /**
-     * For superadminsd
+     * Load optional addional states.
+     * WARNING: Very experimental and dangerous.
      */
-    var findByHost = function(query, callback) {
-      // $log.debug("[RoutesService.findByHost]");
-      var options = {
-        method: 'post',
-        resultIsArray: true
-      };
-      return JLSailsService.resolve('/Routes/findByHost', query, options, callback);
-    };
+    for (var i = 0; i < routes.length; i++) {
+      // if is Main Route
+      if(routes[i].main) {
+        var options = {};
+        // set state main url
+        if(typeof(routes[i].url) === 'string' && routes[i].url.length > 0) {
+          options.url = routes[i].url;
+        }
+        // WARNING custom states are very experimental
+        if(routes[i].customstate === true) {
+          // TODO Dirty hack!
+          if(angular.isDefined(routes[i].state.resolve) && typeof(routes[i].state.resolve) === 'string' && routes[i].state.resolve.length > 0) {
+            /* jshint ignore:start */
+            eval(routes[i].state.resolve);
+            /* jshint ignore:end */
+            options.resolve = resolve;
+          }
+          // TODO Dirty hack!
+          if(angular.isDefined(routes[i].state.views) && typeof(routes[i].state.views) === 'string' && routes[i].state.views.length > 0) {
+            /* jshint ignore:start */
+            eval(routes[i].state.views);
+            /* jshint ignore:end */
+            options.views = view;
+          }
+        // states wich are defined in routeOptions
+        } else {
+          // set state options
+          if(angular.isDefined(routes[i].objectName) && angular.isDefined(routeOptions[routes[i].objectName])) {
+            // console.log(routeOptions[routes[i].objectName]);
+            options.resolve = routeOptions[routes[i].objectName].resolve;
+            options.views = routeOptions[routes[i].objectName].views;
+          } else {
+            console.error("objectName "+routes[i].objectName+" not found for route "+routes[i]);
+          } 
+        }
+        // If options are set, init state
+        if(angular.isDefined(options.url) && angular.isDefined(options.resolve) && angular.isDefined(options.views)) {
+          // console.log("New Route", routes[i].state.name, options);
+          $stateProvider.state(routes[i].state.name, options);
+        }
+        //set alternative urls as redirects
+        if(angular.isArray(routes[i].alternativeUrls)) {
+          for (var a = 0; a < routes[i].alternativeUrls.length; a++) {
+            // e.g. $urlRouterProvider.when('/referenzen', '/referenzen/uebersicht');
+            // console.log("New redirect: "+routes[i].alternativeUrls[a]+" -> "+routes[i].url);
+            $urlRouterProvider.when(routes[i].alternativeUrls[a], routes[i].url);
+          }
+        }
+      } else {
+        // if route is not a main route
+        // if(typeof(routes[i].url) === 'string' && routes[i].url.length > 0) {
+        //   options.url = routes[i].url;
+        //   $urlRouterProvider.when(routes[i].alternativeUrls[a], routes[i].url);
+        // }
+        
+      }
+    }
+  };
 
-    var updateOrCreate = function(route, callback) {
-      $log.debug("[RoutesService.updateOrCreate]", route);
+
+  this.$get = function jlRoutesFactory() {
+    // let's assume that the UnicornLauncher constructor was also changed to
+    // accept and use the useTinfoilShielding argument
+    return new jlRoutes();
+  };
+})
+.filter('showOnlyMainRoutes', function() {
+  // In the return function, we must pass in a single parameter which will be the data we will work on.
+  // We have the ability to support multiple other parameters that can be passed into the filter optionally
+  return function(routes, active) {
+    if(active !== true) {
+      return routes;
+    }
+    var result = [];
+    for (var i = 0; i < routes.length; i++) {
+      if(routes[i].main === true) {
+        result.push(routes[i]);
+      }
+    }
+    return result;
+  };
+})
+.service('RoutesService', function ($rootScope, JLSailsService, $log, SortableService) {
+  var create = function(data) {
+    if(!data || !data.match) {
+      data.match = "";
+    }
+    if(!data || !data.title) {
+      data.title = "";
+    }
+    if(!data.main) {
+      data.main = false;
+    }
+    if(!data || !data.state) {
+      data.state = {};
+    }
+    if(!data || !data.url) {
+      data.url = "";
+    }
+    if(!data || !data.alternativeUrls) {
+      data.alternativeUrls = [];
+    }
+    if(!data.state.name) {
+      data.state.name = "";
+    }
+    if(!data.state.customstate) {
+      data.state.customstate = false;
+    }
+    if(!data.state.url) {
+      data.state.url = "";
+    }
+    if(!data.state.views) {
+      data.state.views = "";
+    }
+    if(!data.state.resolve) {
+      data.state.resolve = "";
+    }
+    if(!data || !data.fallback) {
+      data.fallback = {};
+    }
+    if(!data.fallback.url) {
+      data.fallback.url = "";
+    }
+    return data;
+  };
+
+  var append = function(routes, data, callback) {
+    data = create(data);
+    $log.debug("[RoutesService] data", data);
+    SortableService.append(routes, data, callback);
+  };
+
+  var swap = function(routes, index_1, index_2, callback) {
+    return SortableService.swap(contents, index_1, index_2, callback);
+  };
+
+  var moveForward = function(index, routes, callback) {
+    return SortableService.moveForward(index, routes, callback);
+  };
+
+  var moveBackward = function(index, routes, callback) {
+    return SortableService.moveBackward(index, routes, callback);
+  };
+
+  var removeFromClient = function (routes, index, route, callback) {
+    if(angular.isFunction(callback)) {
+      return SortableService.remove(routes, index, route, callback);
+    }
+    return SortableService.remove(routes, index, route);
+  };
+
+  var remove = function(routes, index, route, callback) {
+    if((angular.isUndefined(route) || route === null) && angular.isDefined(index)) {
+      route = routes[index];
+    }
+    if(angular.isUndefined(index) || index === null) {
+      index = routes.indexOf(route);
+    }
+    routes = removeFromClient(routes, index, route);
+    // if route has an id it is saved on database, if not, not
+    if(route.id) {
+      $log.debug("[RoutesService.remove] remove from server, too", route);
       var options = {
-        method: 'post',
+        method: 'delete',
         resultIsArray: false
       };
-      return JLSailsService.resolve('/Routes/updateOrCreate', {route: route}, options, callback);
-    };
+      return JLSailsService.resolve('/routes/destroy/'+route.id, {id:route.id}, options, callback);
+    }
+  };
 
-    var updateOrCreateByHost = function(host, route, callback) {
-      $log.debug("[RoutesService.updateOrCreateByHost]", host, route);
-      var options = {
-        method: 'post',
-        resultIsArray: false
-      };
-      return JLSailsService.resolve('/Routes/updateOrCreateByHost', {host: host, route: route}, options, callback);
+  var find = function(query, callback) {
+    // $log.debug("[RoutesService.find]");
+    var options = {
+      method: 'post',
+      resultIsArray: true
     };
+    return JLSailsService.resolve('/routes/find', query, options, callback);
+  };
 
-    var updateOrCreateEach = function(routes, callback) {
-      $log.debug("[RoutesService.updateOrCreateEach]", routes);
-      var options = {
-        method: 'post',
-        resultIsArray: true
-      };
-      return JLSailsService.resolve('/Routes/updateOrCreateEach', {routes: routes}, options, callback);
+  var findOne = function(query, callback) {
+    // $log.debug("[RoutesService.find]");
+    var options = {
+      method: 'post',
+      resultIsArray: false
     };
+    return JLSailsService.resolve('/routes/findOne', query, options, callback);
+  };
 
-    var updateOrCreateEachByHost = function(host, routes, callback) {
-      $log.debug("[RoutesService.updateOrCreateEachByHost]", host, routes);
-      var options = {
-        method: 'post',
-        resultIsArray: true
-      };
-      return JLSailsService.resolve('/Routes/updateOrCreateEachByHost', {host: host, routes: routes}, options, callback);
-    };
 
-    return {
-      create: create,
-      append: append,
-      swap: swap,
-      moveForward: moveForward,
-      moveBackward: moveBackward,
-      removeFromClient: removeFromClient,
-      remove: remove,
-      destroy: remove, // Alias
-      find: find,
-      findOne: findOne,
-      findByHost: findByHost,
-      updateOrCreate: updateOrCreate,
-      updateOrCreateByHost: updateOrCreateByHost,
-      updateOrCreateEach: updateOrCreateEach,
-      updateOrCreateEachByHost: updateOrCreateEachByHost,
-      saveEachByHost: updateOrCreateEachByHost, // Alias
+  /**
+   * For superadminsd
+   */
+  var findByHost = function(query, callback) {
+    // $log.debug("[RoutesService.findByHost]");
+    var options = {
+      method: 'post',
+      resultIsArray: true
     };
-  })
-;
+    return JLSailsService.resolve('/Routes/findByHost', query, options, callback);
+  };
+
+  var updateOrCreate = function(route, callback) {
+    $log.debug("[RoutesService.updateOrCreate]", route);
+    var options = {
+      method: 'post',
+      resultIsArray: false
+    };
+    return JLSailsService.resolve('/Routes/updateOrCreate', {route: route}, options, callback);
+  };
+
+  var updateOrCreateByHost = function(host, route, callback) {
+    $log.debug("[RoutesService.updateOrCreateByHost]", host, route);
+    var options = {
+      method: 'post',
+      resultIsArray: false
+    };
+    return JLSailsService.resolve('/Routes/updateOrCreateByHost', {host: host, route: route}, options, callback);
+  };
+
+  var updateOrCreateEach = function(routes, callback) {
+    $log.debug("[RoutesService.updateOrCreateEach]", routes);
+    var options = {
+      method: 'post',
+      resultIsArray: true
+    };
+    return JLSailsService.resolve('/Routes/updateOrCreateEach', {routes: routes}, options, callback);
+  };
+
+  var updateOrCreateEachByHost = function(host, routes, callback) {
+    $log.debug("[RoutesService.updateOrCreateEachByHost]", host, routes);
+    var options = {
+      method: 'post',
+      resultIsArray: true
+    };
+    return JLSailsService.resolve('/Routes/updateOrCreateEachByHost', {host: host, routes: routes}, options, callback);
+  };
+
+  return {
+    create: create,
+    append: append,
+    swap: swap,
+    moveForward: moveForward,
+    moveBackward: moveBackward,
+    removeFromClient: removeFromClient,
+    remove: remove,
+    destroy: remove, // Alias
+    find: find,
+    findOne: findOne,
+    findByHost: findByHost,
+    updateOrCreate: updateOrCreate,
+    updateOrCreateByHost: updateOrCreateByHost,
+    updateOrCreateEach: updateOrCreateEach,
+    updateOrCreateEachByHost: updateOrCreateEachByHost,
+    saveEachByHost: updateOrCreateEachByHost, // Alias
+  };
+});
