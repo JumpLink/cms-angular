c7fe4ae50ec83f6413926694cf1e4c81aa21c66c
JumpLink <pascal@jumplink.eu>
2015-10-29 14:54:20 +0100

    fix medium editor style import

--- src/download/download.js
+++ src/download/download.js
@@ -1,5 +1,11 @@
 // https://github.com/rndme/download
 angular.module('ngDownload', [])
-.service('$download', function () { 
-  return download;
+.service('$download', function ($log) { 
+  if(angular.isDefined(download)) {
+    return download;
+  } else {
+    $log.error("[ngDownload] download is not defined, please install it from https://github.com/rndme/download");
+    return null;
+  }
+  
 });
